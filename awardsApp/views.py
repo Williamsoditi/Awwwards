264c50b1b5fee1e5e0908ac80db634ab20785726
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-11 23:14:26 +0300

    Feat : Updates views and URL

--- awardsApp/views.py
+++ awardsApp/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from .forms import ProjectForm, ProfileForm, UpdateProfileForm
-
-
-
+from .forms import ProjectForm, ProfileForm
+import cloudinary.api
+import cloudinary.uploader
+import cloudinary
 
 # Create your views here.
+def welcome(request):
+    project = Project.objects.all()
+    project = Project.objects.all().order_by('-id')
+    return render(request, 'home.html', {'project': project, 'project': project,})
--- awardsProject/urls.py
+++ awardsProject/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views 
+from django_registration.backends.one_step.views import RegistrationView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('awardsApp.urls')),
+    path('', include('awardsApp.urls')),
 ]
