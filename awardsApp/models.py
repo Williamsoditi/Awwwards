07fca41cf4d0cad74fb8b253d25ededb7715cbfe
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-11 21:37:07 +0300

    Chore : Creates model Project

--- awardsApp/models.py
+++ awardsApp/models.py
@@ -28,3 +28,27 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=600)
+    image = CloudinaryField("Project Image/Screenshot")
+    url = models.URLField(null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def search_project_name(cls, search_term):
+        projects = cls.objects.filter(
+        title__icontains=search_term)
+        return projects 
+
