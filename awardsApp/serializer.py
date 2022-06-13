0b285dd8f42c64bc7872e58413f61a8736373061
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-13 05:13:12 +0300

    Feat : Adds logic to seriallizers

---
+++ awardsApp/serializer.py
@@ -0,0 +1,14 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+from .models import Profile,Project
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("profile_photo","user","bio","location","email","contact")
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ("user","title","description","image","url","date")
