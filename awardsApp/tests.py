3ba5efa733f061456492684be0775f26e4521300
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-16 15:10:45 +0300

    Fix : Updates tests

--- awardsApp/tests.py
+++ awardsApp/tests.py
@@ -10,9 +10,9 @@ class ProjectTestCase(TestCase):
         """
         Create a project for testing
         """
-        self.user=User(username='williams',email='williamsoditi99@@gmail.com',password='qwerty1234')
+        self.user=User(username='williams',email='williamsoditi99@gmail.com',password='qwerty1234')
         self.project=Project(image='SayCheese.jpg',title='SayCheese',description='SayCheese',url='github.com', user=self.user)
-        self.rate=Rating(user=self.user, content=0, usability=0, design=0, average=0)
+        self.rate=Rating(user=self.user, content=0, usability=0, design=0, average=0, project=self.project)
 
 
     def test_instance(self):
@@ -32,13 +32,15 @@ class ProjectTestCase(TestCase):
         self.assertTrue(len(projects) > 0)
         self.assertTrue(len(users) > 0)
         self.assertTrue(len(rates) > 0)
+        
     def test_update(self):
         self.user.save()
         self.project.save_project()
+
     def test_delete(self):
         self.user.save()
         self.project.save_project()
-        self.rate.save_ratings()
+        self.rate.save()
       
         Rating.objects.get(id =self.rate.id).delete()
         Project.objects.get(id =self.project.id).delete()
